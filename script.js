6aa65f8b95809ebad6bf27fce5a192989ad86f9f
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-12-19 22:09:17 -0800

    Add cookie consent banner

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ const copyYear = document.querySelector('[data-year]');
 copyYear.textContent = new Date().getFullYear();
 
 ///////////////////////////////////////
+// Cookie Message
+
+const header = document.querySelector('.header');
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn" data-close-cookie>Got it!</button>';
+header.before(message);
+
+const closeCookie = document.querySelector('[data-close-cookie]');
+closeCookie.addEventListener('click', function(){
+  message.remove();
+});
+
+///////////////////////////////////////
 // Modal window
 
 const modal = document.querySelector('[data-modal]');
