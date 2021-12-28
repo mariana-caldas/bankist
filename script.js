f212c063bd34ba86f50fa5591e76c91c2ea92ed6
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-12-27 20:16:17 -0800

    Add menu fade animation

--- script.js
+++ script.js
@@ -46,6 +46,26 @@ navLinksWrapper.addEventListener('click', function(e){
   }
 });
 
+// Menu fade animation
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e) {
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    
+    siblings.forEach(el => {
+     if (el !== link ) el.style.opacity = this;
+    });
+
+    logo.style.opacity = this;
+  }
+}
+//Setting "this" manually through "bind" to pass the opacity as an "argument" into the handler function
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 ///////////////////////////////////////
 // Tabbed component
 
