c6488b4bf4fdb3e75d159712f67b04c496c87ef6
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2021-12-28 23:11:25 -0800

    Add lazy loading images with Intersection Observer API

--- script.js
+++ script.js
@@ -116,6 +116,36 @@ allSections.forEach( function(section){
 })
 
 ///////////////////////////////////////
+// Lazy loading images with Intersection Observer API
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer){
+ const [entry] = entries;
+
+ if (!entry.isIntersecting) return;
+
+ //Replace src with data-src
+ entry.target.src = entry.target.dataset.src;
+
+ entry.target.addEventListener('load', function() {
+  entry.target.classList.remove('lazy-img');
+ });
+
+ observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px' //Make images load before the thresold is reached
+})
+
+imgTargets.forEach(img => {
+  imgObserver.observe(img);
+})
+
+///////////////////////////////////////
 // Tabbed component
 
 const tabTrigger = document.querySelectorAll('[data-tab]');
